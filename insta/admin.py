3944716e9f9c5da1a46d5c8c57d11506ca92e2b8
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 12:38:22 +0300

    importing models to admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Editor,Comments,Profile
+from .models import Image,Comments,Profile
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Comments)
