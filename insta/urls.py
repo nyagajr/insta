840aca3c27b3c725324e79962d3e7e42b4870915
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 17:31:32 +0300

    routing for logout

--- insta/urls.py
+++ insta/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
@@ -5,7 +6,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
