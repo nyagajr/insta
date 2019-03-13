ddf9bb01251dabe50dc4d2b3bf971ed2a41ad7d8
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 10:56:01 +0300

    testing

--- insta/views.py
+++ insta/views.py
@@ -4,3 +4,7 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+def index(request):
+    return render(request, 'all-pages/index.html')
