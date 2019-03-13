fd23e72768672383472ec3c7f81babad7f770f76
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 12:02:55 +0300

    adding a date to which the comment was written

--- insta/models.py
+++ insta/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class Profile(models.Model):
     profile_photo = models.CharField(max_length =30)
     Bio = models.CharField(max_length =30)
+    def __str__(self):
+        return self.profile_photo
 
 class Comments(models.Model):
     comment = models.CharField(max_length =30)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.comment
 
 class Image(models.Model):
     image = models.CharField(max_length =30)
@@ -14,3 +19,6 @@ class Image(models.Model):
     image_caption = models.CharField(max_length =30)
     image_profile = models.ForeignKey(Profile)
     img_comments = models.ForeignKey(Comments)
+
+    def __str__(self):
+        return self.image_name
