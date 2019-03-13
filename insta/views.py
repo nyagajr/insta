48dec6cac9b2649cafbfc32e9eeb0e3103146bb9
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 12:39:24 +0300

    importing models to view

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
+from .models import Profile,Comments,Image
 
 # Create your views here.
 def welcome(request):
