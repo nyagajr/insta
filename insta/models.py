14e8a09a2ffd84e6642959433e92388a1e24ba19
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 11:55:06 +0300

    adding profile and comments class

--- insta/models.py
+++ insta/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.CharField(max_length =30)
+    Bio = models.CharField(max_length =30)
+
+class Comments(models.Model):
+    comment = models.CharField(max_length =30)
+
 class Image(models.Model):
     image = models.CharField(max_length =30)
     image_name = models.CharField(max_length =30)
     image_caption = models.CharField(max_length =30)
-    image_profile = models.ForeignKey(profile)
+    image_profile = models.ForeignKey(Profile)
+    img_comments = models.ForeignKey(Comments)
