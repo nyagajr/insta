a5afea8c84d0ab2fae46a50a7fd6a2683155ad36
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 17:30:36 +0300

    newsletter class

--- insta/models.py
+++ insta/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# class NewsLetterRecipients(models.Model):
+#     name = models.CharField(max_length = 30)
+#     email = models.EmailField()
+
 class Profile(models.Model):
-    profile_photo = models.CharField(max_length =30)
+    profile_photo = models.ImageField(upload_to = 'images/')
     Bio = models.CharField(max_length =30)
     def __str__(self):
         return self.profile_photo
@@ -16,9 +20,13 @@ class Comments(models.Model):
 class Image(models.Model):
     image_name = models.CharField(max_length =30)
     image_caption = models.CharField(max_length =30)
-    image_profile = models.ForeignKey(Profile)
+    # image_profile = models.ForeignKey(Profile)
     img_comments = models.ForeignKey(Comments)
     image = models.ImageField(upload_to = 'images/')
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
 
     def __str__(self):
         return self.image_name
