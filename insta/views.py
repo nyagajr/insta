f34f47e9de6461fcecfe8a34a9cdd594d18fa2cd
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 16:27:25 +0300

    changing the routing

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,4 @@
+from .forms import NewsLetterForm
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
@@ -5,8 +6,30 @@ from .models import Profile,Comments,Image
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    query_img = Image.objects.all()
+    return render(request, 'welcome.html', {"query_img":query_img})
 
 
 def index(request):
     return render(request, 'all-pages/index.html')
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-pages/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-pages/search.html',{"message":message})
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'templates/welcome.html', {"letterForm":form})
