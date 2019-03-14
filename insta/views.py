557aacff80fd0696d4386db024a51eadc22a205a
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-14 16:23:06 +0300

    deployment

--- insta/views.py
+++ insta/views.py
@@ -4,13 +4,37 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
 from .models import Profile,Comments,Image
+from .forms import CommentsForm
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'comments.html', {'form': form})
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
+
+
 def welcome(request):
     query_img = Image.objects.all()
     return render(request, 'welcome.html', {"query_img":query_img})
 
+def signup(request):
+    return render(request, 'registration/registration_form.html')
 
 def index(request):
     return render(request, 'all-pages/index.html')
