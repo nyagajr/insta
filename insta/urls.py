f6cf99593f261e547fb0443aec48cd2959970ba9
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-14 11:15:15 +0300

    adding a comment url

--- insta/urls.py
+++ insta/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^search/', views.search_results, name='search_results'),
-    # url(r'^logout/$', views.logout, {"next_page": '/'}),
+    # url(r'^Comments/', views.Comments, name='comment'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
