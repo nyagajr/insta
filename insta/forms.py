99d238cbcc0e15be3b96e611bc0274f4a4c52a5e
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-14 16:35:33 +0300

    deployment

---
+++ insta/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+
+class CommentsForm(forms.Form):
+    comments = forms.CharField(label='comment',max_length=100)
